4322aea18ef2a1b33bc7a23ff89db20337ccd935
nihil <nihil@archon-dev.(none)>
2012-12-08 20:13:23 +0000

    Added absolute basic UI.

---
+++ templates/index.php
@@ -0,0 +1,48 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Blinkey I/O</title>
+    <link href="../static/css/bootstrap.css" rel="stylesheet">
+    <link href="../static/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="../static/css/docs.css" rel="stylesheet">
+    <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	<script src="../static/js/jquery-ui-1.9.2.custom.min.js"></script>
+<div class="container" style="height: 70px;">
+	<div class="page-header">
+		<h1>Blinkey I/O</h1>
+	</div>
+	<div class="row-fluid well">
+		<h4>Set your LEDs:</h4>
+		<div class="padding10">
+		<img src="../static/images/led_on.png"/>	
+		<img src="../static/images/led_on.png"/>	
+		<img src="../static/images/led_on.png"/>	
+		<img src="../static/images/led_on.png"/>	
+		<img src="../static/images/led_on.png"/>	
+		<img src="../static/images/led_on.png"/>	
+		<img src="../static/images/led_on.png"/>	
+		<a href="#" class="btn btn-action" style="float:right;" >Set</a>
+		</div>
+	</div>
+	<div class="row-fluid well">
+		<div class="span4" style="background:white;">
+		<h5>Drag the blocks to the queve:</h5>
+		
+		</div>
+
+		<div class="span6" style="background:white;height:400px;">
+		
+		</div>
+	</div>
+</div>
+</body>
+</html>
+
+
