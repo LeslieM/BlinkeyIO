85668e32d9bc89edc4785611665fc9211cf5ea2b
nihil <nihil@archon-dev.(none)>
2012-12-09 10:50:06 +0000

    Added led_off image, some minor wrapping of page elements, added image for unknown.

--- templates/index.php
+++ templates/index.php
@@ -18,53 +18,47 @@
 
 </head>
 <body>
-		    <style>
-		    #draggable { width: 100px; height: 100px; padding: 0.5em; float: left; margin: 10px 10px 10px 0; }
-		    #droppable { width: 150px; height: 150px; padding: 0.5em; float: left; margin: 10px; }
-		select#gender option[value="male"] {background-image:url(/static/images/and.jpg);}
-		select#gender option[value="female"] {background-image:url(/static/images/or.jpg);}
-		select#gender option[value="others"] {background-image:url(and.jpg);}
-		    </style>
 <div class="container" style="height: 70px;">
-	<div class="page-header">
+	<div class="page-header">		
 		<h1>Blinkey I/O</h1>
 	</div>
 	<div class="row-fluid well">
-		<h4>Set your LEDs:</h4>
-		<div class="padding10">
+		<h4>Pattern to create:</h4>
+		<div class="padding10 well span5" style="background:grey;">
+		<img src="/static/images/led_on.png"/>	
+		<img src="/static/images/led_off.png"/>	
 		<img src="/static/images/led_on.png"/>	
 		<img src="/static/images/led_on.png"/>	
+		<img src="/static/images/led_off.png"/>	
+		<img src="/static/images/led_off.png"/>	
 		<img src="/static/images/led_on.png"/>	
-		<img src="/static/images/led_on.png"/>	
-		<img src="/static/images/led_on.png"/>	
-		<img src="/static/images/led_on.png"/>	
-		<img src="/static/images/led_on.png"/>	
-		<img src="/static/images/led_on.png"/>	
+		<img src="/static/images/led_off.png"/>	
+		<!--
 		<a href="#" class="btn btn-info" style="float:right;" >Set</a>
+		-->
 		</div>
 	</div>
 	<div class="row-fluid well">
-		<div class="span4" style="background:white;">
+		<div class="span4 well" style="background:white;">
 		<h5>Drag the blocks to the queve:</h5>
 		
-		<div id="draggable" class="ui-widget-content">
+		<div class="ui-widget-content">
 			<img src="/static/images/and.jpg"/>
 		</div>
 
 		</div>
-		<div class="span6 row" style="background:white;height:400px;">
- 
+		<div class="span6 row well" style="background:white;height:400px;">
 		    <div class="btn-group">
-    <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-	Select <br/>operator<br/>
-	<span class="caret"></span>
-    </a>
-    <ul class="dropdown-menu">
-	<li><img src="/static/images/and.jpg"/></li>
-	<li><img src="/static/images/or.jpg"/></li>
-	<li><img src="/static/images/xor.jpg"/></li>
-    </ul>
-    </div>
+			    <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+				Select <br/>operator<br/>
+				<span class="caret"></span>
+			    </a>
+		    <ul class="dropdown-menu">
+	            <li><img src="/static/images/and.jpg"/></li>
+		    <li><img src="/static/images/or.jpg"/></li>
+  		    <li><img src="/static/images/xor.jpg"/></li>
+		    </ul>
+		    </div>
 
 		</div>
 	</div>
